6d0ab530a3d77febf234e4b20747c96ecba1dd23
Felix Ruzzoli <felix.ruzzoli@lrx.gmbh>
2021-11-07 23:45:28 +0100

    [FIX] Some cleanup and making sure the mod is winnable

--- create.release.stripped.sh
+++ create.release.stripped.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-if bumpversion $@ --tag --commit; then
-    cd ..
-    7z a -r -xr@./Vigilo_Confido/release.stripped.exclude Vigilo_Confido_S.zip Vigilo_Confido/
-    cd Vigilo_Confido
-fi
+#!/bin/bashif bumpversion $@ --tag --commit; then    cd ..    7z a -r -xr@./Vigilo_Confido/release.stripped.exclude Vigilo_Confido_S.zip Vigilo_Confido/    cd Vigilo_Confidofi
